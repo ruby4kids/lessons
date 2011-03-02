e862f96df2ba559b9daf6d14558b4b08f72e3799
Alberto Morales <alberto@moralitos.com>
2011-03-02 00:08:29 -0500

    pausing the game

--- my_game_folder/my_game.rb
+++ my_game_folder/my_game.rb
@@ -8,31 +8,47 @@ class MyGame < Gosu::Window
     super(300, 300, false)
     @player1 = Player.new(self)
     @ball = Ball.new(self)
+    @pause = false
+    @font = Gosu::Font.new(self, 'System', 24)
+  end
+  
+  def button_down(id)
+    if id == Gosu::Button::KbP
+      if @pause == false
+        @pause = true
+      else
+        @pause = false
+      end
+    end
   end
   
   def update
-    if button_down? Gosu::Button::KbLeft
-      @player1.move_left
+    if @pause == false
+      if button_down? Gosu::Button::KbLeft
+        @player1.move_left
+      end
+
+      if button_down? Gosu::Button::KbRight
+        @player1.move_right
+      end
+
+      if button_down? Gosu::Button::KbUp
+        @player1.move_up
+      end
+
+      if button_down? Gosu::Button::KbDown
+        @player1.move_down
+      end
+
+      @ball.update   
     end
-    
-    if button_down? Gosu::Button::KbRight
-      @player1.move_right
-    end
-    
-    if button_down? Gosu::Button::KbUp
-      @player1.move_up
-    end
-    
-    if button_down? Gosu::Button::KbDown
-      @player1.move_down
-    end
-    
-    @ball.update
+
   end
   
   def draw
     @player1.draw
     @ball.draw
+    @font.draw("The game is paused.", 50, 200, 10) if @pause == true
   end
   
 end
