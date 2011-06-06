fad4e3b2fb6eb67ba9b55dc7bb4f0b4673e1fb70
Bill Davenport <bill.davenport@livingsocial.com>
2011-06-06 19:31:26 -0400

    added lesson pdf and sample rails app

---
+++ rails/lesson_03/twitter_for_zombies/README.md
@@ -0,0 +1,13 @@
+1. *Getting Started (designed for Macintosh OS X*)*
+	rake db:migrate
+
+2. Start the Web Server. BTW: To stop the server press Control-C combination on the keyboard.
+	rails s
+
+Open a browser and go to this address:
+http://localhost:3000/tweets
+
+Start the tutorial. 
+
+
+* For Window's users install http://rubyinstaller.org/. This should work but this has not been tested.
---
+++ rails/lesson_03/twitter_for_zombies/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterForZombies::Application.config.secret_token = 'ca6a12921d1f6e2b7390919e83e79b09388ffd67df51af4601160445afe956b92dd482f4aa2f8dc3b8e14dc4c1e722ad7374b56e0124f804c7dabbb857296bdd'
